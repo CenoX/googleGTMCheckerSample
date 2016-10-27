2d99931efa260dba3699c1eccef213f7155cbfbc
CenoX <cenox@CenoXui-MacBook-Pro.local>
2016-10-28 04:39:23 +0900

    first init

---
+++ googleGTMCheckerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  googleGTMCheckerSample
+//
+//  Created by CenoX on 2016. 10. 28..
+//  Copyright © 2016년 Team ArcStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
